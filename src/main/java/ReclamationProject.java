b5cab3352795cb3850c52ca70737d9eb94cd918e
jeffreylin17 <jeffrey6199@gmail.com>
2017-09-26 18:22:12 -0500

    Lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,4 +1,4 @@
-/*
+/**
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
@@ -8,19 +8,34 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    /**
+     *@param a : jaksdf
+     *@param b : sfjkh
+     *@return r : sfaf
+     */
+    public static String doitt(final String a, final String b) {
+        String temp1 = a;
+        String temp2 = b;
+        if (a.length() > b.length()) {
+            String c = a; //set c to a
+            temp1 = b;
+            temp2 = c;
+            }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < temp1.length(); i++) {
+            for (int j = temp1.length() - i; j > 0; j--) {
+                for (int k = 0; k < temp2.length() - j; k++) {
+                    if (temp1.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = temp1.substring(i, i + j);
+
+                    }
+                }
+            }
+        }
+        return r;
+    }
 }
